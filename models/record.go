840da789f3a42cd8649595d0a4b307ba790b4273
jackidu14 <vincent_b2010@hotmail.fr>
2025-01-28 15:18:14 +0100

    Adding db unmarshal & json model marshal, using RecentlyRecords model for recently route

--- models/record.go
+++ models/record.go
@@ -3,11 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Record struct {
-	Id          primitive.ObjectID `json:"_id,omitempty"`
-	NativImgSrc string             `json:"NativImgSrc,omitempty" validate:"required"`
-	MidImgSrc   string             `json:"MidImgSrc,omitempty" validate:"required"`
-	ThumbImgSrc string             `json:"ThumbImgSrc,omitempty" validate:"required"`
-	Description string             `json:"Description,omitempty"`
-	Location    string             `json:"Location,omitempty" validate:"required"`
-	Date        primitive.DateTime `json:"Date,omitempty" validate:"required"`
+	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	NativImgSrc string             `json:"nativImgSrc,omitempty" validate:"required"`
+	MidImgSrc   string             `json:"midImgSrc,omitempty" validate:"required"`
+	ThumbImgSrc string             `json:"thumbImgSrc,omitempty" validate:"required"`
+	Description string             `json:"description,omitempty"`
+	Location    string             `json:"location,omitempty" validate:"required"`
+	Date        primitive.DateTime `json:"date,omitempty" validate:"required"`
 }
--- pkg/recently/get-recently.go
+++ pkg/recently/get-recently.go
@@ -11,10 +11,10 @@ import (
 )
 
 func GetRecentlyContent(c *gin.Context) {
-	var records []models.Record
-
+	/* Connecting to recently collection */
 	collection := connector.GetCollection("recently")
-	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	/* Getting collections. bson.D{} stands for "no filter" */
+	cursor, err := collection.Find(context.Background(), bson.D{})
 
 	if err != nil {
 		fmt.Printf("%s\n", err)
@@ -23,6 +23,75 @@ func GetRecentlyContent(c *gin.Context) {
 	}
 	defer cursor.Close(context.Background())
 
-	cursor.All(context.Background(), &records)
-	c.JSON(200, records)
+	/* Extracting & unmarshall database collections */
+	var documents []models.Record
+	err = cursor.All(context.Background(), &documents)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		c.JSON(500, gin.H{"error::database": "Error while reading database - unable to extract recently records"})
+		return
+	}
+
+	for _, doc := range documents {
+		fmt.Printf("Date: %s\n", doc.Date.Time())
+	}
+
+	/* Formatting as expected */
+	extractedRecords, err := extractRecentlyRecords(documents)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		c.JSON(500, gin.H{"error::database": "Error while reading data - unable to re-order records"})
+		return
+	}
+
+	c.JSON(200, extractedRecords)
+}
+
+func extractRecentlyRecords(records []models.Record) (models.RecentlyRecords, error) {
+	var recentlyRecords models.RecentlyRecords
+	lastlyRecord, err := getLastlyRecord(records)
+
+	if err != nil {
+		return recentlyRecords, err
+	}
+
+	/* 2 weeks before last records */
+	dateDelimiter := lastlyRecord.Date.Time().AddDate(0, 0, -14) // 24 * 14
+
+	fmt.Printf("Date to delimit: %s\n", dateDelimiter)
+
+	for _, rec := range records {
+		if rec.Date.Time().Before(dateDelimiter) {
+			fmt.Printf("Adding %s in lately\n", rec.Date.Time())
+			recentlyRecords.Lately = append(recentlyRecords.Lately, rec)
+		} else {
+			fmt.Printf("Adding %s in lastly\n", rec.Date.Time())
+			recentlyRecords.Lastly = append(recentlyRecords.Lastly, rec)
+		}
+	}
+
+	return recentlyRecords, nil
+}
+
+func getLastlyRecord(records []models.Record) (models.Record, error) {
+	var result models.Record
+
+	if len(records) == 0 {
+		return result, fmt.Errorf("getLastlyRecord: no records to walk in")
+	}
+	result = records[0]
+
+	for key, rec := range records {
+		if key == 0 {
+			continue
+		}
+
+		if rec.Date.Time().After(result.Date.Time()) {
+			result = rec
+		}
+	}
+
+	return result, nil
 }
