4a7ca3b8d062a2804b2a8df92c9cafa4738ef8a4
jackidu14 <vincent_b2010@hotmail.fr>
2025-03-18 11:53:42 +0100

    [FRONT]: Cleaning unused files

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,47 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Recently from '@/views/Recently.vue'
-import WelcomeItem from '@/components/WelcomeItem.vue'
-import TheWelcome from '@/components/TheWelcome.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export type RoutesNames = 'RECENT' | 'DATE' | 'LOCATION' | 'PINED'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
     {
       path: '/',
       name: 'RECENT',
-      component: Recently,
+      component: () => import('@/views/Recently.vue'),
+      meta: {
+        layout: 'guest'
+      }
     },
     {
       path: '/per-date',
       name: 'DATE',
-      component: HelloWorld,
+      component: () => import('@/views/Recently.vue'),
+      meta: {
+        layout: 'guest'
+      }
     },
     {
       path: '/per-location',
       name: 'LOCATION',
-      component: WelcomeItem,
+      component: () => import('@/views/Recently.vue'),
+      meta: {
+        layout: 'guest'
+      }
     },
     {
       path: '/pin',
       name: 'PINED',
-      component: TheWelcome,
-    },
+      component: () => import('@/views/Recently.vue'),
+      meta: {
+        layout: 'guest'
+      }
+    }
   ],
 })
 
