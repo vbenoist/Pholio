7bd523a7eee752079fa2de49425826cb2272d6f1
jackidu14 <vincent_b2010@hotmail.fr>
2025-06-30 17:04:40 +0200

    [BACK]: Avoid null result in paginated documents prop

--- internal/controllers/record/location.go
+++ internal/controllers/record/location.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/vbenoist/pholio/internal/helpers/controller"
+	apimodels "github.com/vbenoist/pholio/internal/models/api"
 	"github.com/vbenoist/pholio/internal/services/record"
 )
 
@@ -19,6 +20,14 @@ func GetRecordsPerLocation(c *gin.Context) {
 		fmt.Printf("record::GetRecordsPerDate %s\n", err.Error())
 		c.JSON(500, gin.H{"error::database": "Error while reading database - unable to get records"})
 		return
+	}
+
+	/*
+		Avoid "null" response. An empty array will result in [] after json marshalling.
+		A simple declaration of an empty array will result in "null"
+	*/
+	if len(paginatedResult.Documents) == 0 {
+		paginatedResult.Documents = []apimodels.GroupbyRecord{}
 	}
 
 	c.JSON(200, paginatedResult)
