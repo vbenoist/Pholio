c995a2c0f81aec1f6121ab87d29b5a5f93e3925a
jackidu14 <vincent_b2010@hotmail.fr>
2025-07-01 12:03:04 +0200

    [FRONT]: Reworking detailed record thumb resolving

--- frontend/src/stores/detailedContent.ts
+++ frontend/src/stores/detailedContent.ts
@@ -1,8 +1,11 @@
 import type { Ref } from 'vue'
 import { inject, ref } from 'vue'
 import { defineStore } from 'pinia'
+import { cloneDeep } from 'lodash'
 import type { DetailedRecord } from '@/models/record'
-import { ApiResolver } from '@/plugins/apiResolver'
+import type { PaginatedResults, PaginationQuery } from '@/models/api'
+import { UploadableFile } from '@/models/uploadableFile'
+import { ApiResolver, type ApiResolverCallable } from '@/plugins/apiResolver'
 import PaginatedStore from '@/composables/store/pagination'
 import { mergeDetailedResults } from '@/helpers/store/merge'
 
@@ -10,13 +13,47 @@ export const useDetailedStore = defineStore('detailedContent', () => {
   const apiResolver = inject('$apiResolver') as ApiResolver
   const content: Ref<Array<DetailedRecord>> = ref([])
 
+  const customResolver: ApiResolverCallable = async (pageParams: Partial<PaginationQuery> | null): Promise<PaginatedResults<DetailedRecord> | null> => {
+    const apiResults = await apiResolver.fetchDetailed(pageParams)
+    if(!apiResults) return null
+
+    const prs: Promise<void>[] = []
+
+    apiResults.documents.forEach(detRecord => {
+      prs.push(new Promise((resolve, _) => {
+        resolveThumbRecord(cloneDeep(detRecord))
+          .then(drWithThumb => {
+            const idx = apiResults.documents.findIndex((dr) => dr.id === drWithThumb.id)
+            if(idx === -1) return
+            apiResults.documents[idx] = drWithThumb
+            resolve()
+          })
+      }))
+    })
+
+    await Promise.all(prs)
+    return apiResults
+  }
+
+  const resolveThumbRecord = async(record: DetailedRecord): Promise<DetailedRecord> => {
+    const imgThumb = await apiResolver.getLinkedThumb(record.id)
+    if(!imgThumb) return record
+
+
+    return {
+      ...record,
+      status: 'PENDING',
+      file: new UploadableFile(imgThumb)
+    }
+  }
+
   const { fetched,
     hasFetched,
     fetchContent,
     fetchNextContent,
     getContent,
     initContent
-  } = PaginatedStore(apiResolver.fetchDetailed, mergeDetailedResults, content)
+  } = PaginatedStore(customResolver, mergeDetailedResults, content)
 
   return {
     fetched,
--- frontend/src/views/Admin/RecordManage.vue
+++ frontend/src/views/Admin/RecordManage.vue
@@ -1,75 +1,30 @@
 <template>
   <div :ref="containerRefName">
     <span v-if="fetched">
-      <ManagePublications v-model="records" />
+      <ManagePublications v-model="apiDetailedRecord" />
     </span>
     <span v-else>Loading...</span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, inject, ref, watch } from 'vue'
+import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
-import { cloneDeep } from 'lodash'
 import scrollHook from '@/composables/scrollHook'
 import ManagePublications from '@/components/Admin/Manage/ManagePublications.vue'
 import { useDetailedStore } from '@/stores/detailedContent'
-import type { ApiResolver } from '@/plugins/apiResolver'
-import { type DetailedRecord as ApiDetailedRecord } from '@/models/api/detailed-record'
-import { type DetailedRecord } from '@/models/record'
-import { UploadableFile } from '@/models/uploadableFile'
-
-const apiResolver = inject('$apiResolver') as ApiResolver
 const detailedApiContentStore = useDetailedStore()
 const { fetched } = storeToRefs(detailedApiContentStore)
 const { containerRefName, registerHook } = scrollHook()
 
 const apiDetailedRecord = computed(() => detailedApiContentStore.getContent())
-const records = ref<Array<DetailedRecord>>([])
-const workingRecords = ref<Array<string>>([])
 
 const resolveContent = async () => {
   await detailedApiContentStore.fetchContent()
 }
 
-const onRecordsResolve = async () => {
-  const prs: Promise<void>[] = []
-
-  apiDetailedRecord.value.forEach (adrc => {
-    const rcIdx = records.value.findIndex((rc) => rc.id === adrc.id)
-    const wkIdx = workingRecords.value.findIndex((id) => id === adrc.id)
-
-    if(rcIdx === -1 && wkIdx === -1) {
-      workingRecords.value.push(adrc.id)
-      prs.push(completeDetailedRecord(cloneDeep(adrc)))
-    }
-  })
-
-  await Promise.all(prs)
-}
-
-const completeDetailedRecord = async(record: ApiDetailedRecord): Promise<void> => {
-  workingRecords.value.push(record.id)
-
-  /* Resolving record thumb */
-  console.log("resolving: ", record.id)
-  const imgThumb = await apiResolver.getLinkedThumb(record.id)
-  if(!imgThumb) return
-
-  records.value.push({
-    ...record,
-    date: new Date(record.date),
-    status: 'PENDING',
-    file: new UploadableFile(imgThumb)
-  })
-}
-
 registerHook(() => {
   detailedApiContentStore.fetchNextContent()
-})
-
-watch(apiDetailedRecord, () => {
-  onRecordsResolve()
 })
 
 resolveContent()
